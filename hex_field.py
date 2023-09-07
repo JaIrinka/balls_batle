7a389003f6b137ad020904360297bb00a955af4b
Imolodykh <imolodykh@eqvantalab.ru>
2023-09-07 20:21:42 +0300

    add menu and square field

--- hex_field.py
+++ hex_field.py
@@ -1,48 +1,98 @@
 from math import pi as PI, cos, sin, sqrt
+from configure import WIDTH, HEIGHT
 
 
-def hex_field(canvas, size, max_x, mav_y):
-    hexagons = []
-    for center in get_center(size, max_x, mav_y):
-        hexagon = make_hexagon(center, size)
-        points = []
-        for p in hexagon:
-            points.extend(p.get_coords())
-        hexagons.append(canvas.create_polygon(
-            *points, fill="orange", width=0 #, outline="black"
-        ))
-    return hexagons
+class HexField:
+    def __init__(self, canvas, size=10):
+        self.size = size
+        self.canvas = canvas
+
+    def field(self):
+        hexagons = []
+        for center in self.get_center():
+            hexagon = self.make_hexagon(center)
+            points = []
+            for p in hexagon:
+                points.extend(p.get_coords())
+            hexagons.append(self.canvas.create_polygon(
+                *points, fill="orange", width=0, outline="black"
+            ))
+        return hexagons
 
 
-def get_center(size, max_x, mav_y):
-    top_offset = sqrt(3)/2 * size
-    r = 0
-    while top_offset <= mav_y:
-        if not r:
-            left_offset = sqrt(3)/2 * size
-            r = 1
-        else:
+    def get_center(self):
+        top_offset = sqrt(3)/2 * self.size
+        r = 0
+        while top_offset < HEIGHT + 2*self.size:
+            if not r:
+                left_offset = sqrt(3)/2 * self.size
+                r = 1
+            else:
+                left_offset = 0
+                r = 0
+            while left_offset < WIDTH + 2*self.size:
+                center = Point(left_offset, top_offset)
+                left_offset += sqrt(3) * self.size
+                yield center
+            top_offset += sqrt(3)/4 * self.size + self.size 
+
+
+    def make_hexagon(self, center):
+        hexagon = []
+        for j in range(6):
+            hexagon.append(self.hex_corner(center, j))
+        return hexagon
+
+
+    def hex_corner(self, center, i):
+        angle_deg = 60 * i + 30
+        angle_rad = PI / 180 * angle_deg
+        return Point(center.x + self.size * cos(angle_rad), center.y + self.size * sin(angle_rad))
+
+class SquareField:
+    def __init__(self, canvas, size=15):
+        self.size = size
+        self.canvas = canvas
+
+    def field(self):
+        squares = []
+        for square in self.make_squares():
+            points = []
+            for p in square:
+                points.extend(p.get_coords())
+            squares.append(self.canvas.create_rectangle(
+                *points, fill="orange", width=0, outline="black"
+            ))
+        return squares
+
+
+    def get_lt(self):
+        top_offset = sqrt(3)/2 * self.size
+        r = 0
+        while top_offset < HEIGHT + 2*self.size:
+            if not r:
+                left_offset = sqrt(3)/2 * self.size
+                r = 1
+            else:
+                left_offset = 0
+                r = 0
+            while left_offset < WIDTH + 2*self.size:
+                center = Point(left_offset, top_offset)
+                left_offset += sqrt(3) * self.size
+                yield center
+            top_offset += sqrt(3)/4 * self.size + self.size 
+
+
+    def make_squares(self):
+        top_offset = 0
+        while top_offset < HEIGHT + self.size:
             left_offset = 0
-            r = 0
-        while left_offset <= max_x:
-            center = Point(left_offset, top_offset)
-            left_offset += sqrt(3) * size
-            yield center
-        top_offset += sqrt(3)/4 * size + size 
-
-
-def make_hexagon(center, size):
-    hexagon = []
-    for j in range(6):
-        hexagon.append(hex_corner(center, size, j))
-    return hexagon
-
-
-def hex_corner(center, size, i):
-    angle_deg = 60 * i + 30
-    angle_rad = PI / 180 * angle_deg
-    return Point(center.x + size * cos(angle_rad), center.y + size * sin(angle_rad))
-
+            while left_offset < WIDTH + self.size:
+                lt = Point(left_offset, top_offset)
+                rb = Point(left_offset + self.size, top_offset + self.size)
+                left_offset += self.size
+                yield [lt, rb]
+            top_offset += self.size
 
 class Point:
     def __init__(self, x=0, y=0):
@@ -51,20 +101,6 @@ class Point:
 
     def __repr__(self):
         return f"Point({self.x!r}, {self.y!r})"
-
-    # def __abs__(self):
-    #     return hypot(self.x, self.y)
-
-    # def __bool__(self):
-    #     return bool(abs(self))
-
-    # def __add__(self, other):
-    #     x = self.x + other.x
-    #     y = self.y + other.y
-    #     return Point(x, y)
-
-    # def __mul__(self, scalar):
-    #     return Point(self.x * scalar, self.y * scalar)
 
     def get_coords(self):
         return self.x, self.y
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # import time
 from tkinter import *
-from hex_field import hex_field
+from tkinter import ttk
+from hex_field import HexField, SquareField
+from configure import WIDTH, HEIGHT
 import random
 
 root = Tk()
 root.title("PRINTING")
+root.geometry(f"{WIDTH}x{HEIGHT}")
+root.resizable(False, False)
 
-max_x = 1000
-mav_y = 600
-size_hex = 20
+size_hex = 15
 field_colors = [
     "gold",
     "yellow",
@@ -29,76 +31,45 @@ field_colors = [
     "lightcoral",
     "lightpink",
     "lightsalmon",
-    "tomato"
+    "tomato",
 ]
 
-canvas = Canvas(
-    root, width=max_x, height=mav_y, bg="white"
-)  ##Размер листа и его цвет, на котором будут графические изображения
-canvas.pack()
 
-hexagons = hex_field(canvas, size_hex, max_x, mav_y)
+def print_hex():
+    canvas = Canvas(
+        root, width=WIDTH, height=HEIGHT, bg="white"
+    )
+    canvas.pack()
 
-# canvas.create_line(10, 10, 500, 500, fill="green", width=2)
-# canvas.create_line(10, 10, 490, 245, 10, 490, 10, 10, fill="red", width=5)
-# canvas.create_rectangle(100, 100, 300, 200, outline="blue", fill="red", width=10)
-# canvas.create_rectangle(150, 210, 250, 400, outline="green", fill="yellow", width=10)
-# canvas.create_rectangle(260, 350, 300, 400, fill="grey", width=10)
+    def remove_canvas(a):
+        canvas.destroy()
+        hexBtn.pack()
 
-# canvas.create_oval(100, 100, 400, 300, fill="red", outline="black")
-# canvas.create_oval(50, 50, 250, 250, outline="green", fill="yellow", width=10)
-# canvas.create_oval(100, 100, 200, 200, outline="blue", fill="red", width=10)
-ball = canvas.create_oval(900, 500, 1000, 600, fill="green", width=5)
+    canvas.bind("<ButtonPress>", remove_canvas)
+    hexBtn.pack_forget()
 
-# canvas.create_polygon(100, 110, 10, 210, 250, 400, outline="green", fill="yellow", width=10)
-# canvas.create_polygon(100, 100, 100, 200, 200, 200, 200, 100, outline="blue", fill="blue")
-# canvas.create_polygon(100, 100, 100, 200, 300, 300, 10, 50, outline="blue", fill="orange", width=10)
-# canvas.create_polygon(200, 150, 200, 250, 250, 300, 350, 300, 400, 250, 400, 150, 350, 100, 250, 100, outline="lime", fill="aqua", width=10)
+    field = HexField(canvas=canvas)
+    # field = SquareField(canvas=canvas)
+    
+    figures = field.field()
 
-# p1 = [300, 200]
-# p2 = [250, 300]
-# p3 = [350, 300]
-# p4 = [350, 100]
-# p5 = [250, 100]
-# canvas.create_polygon(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1], p1[0], p1[1], p4[0], p4[1], p5[0], p5[1], outline="brown", fill="tan", width=10)
-
-# f2 = [450, 300]
-# f3 = [550, 300]
-# f4 = [550, 100]
-# f5 = [450, 100]
-# canvas.create_polygon(f2[0], f2[1], f3[0], f3[1], f5[0], f5[1], f4[0], f4[1], outline="brown", fill="tan", width=10)
-
-# canvas.create_text(250, 250, font="Arial 50", text="Hello World", fill="green")
-
-# canvas.create_oval(100, 100, 400, 300, fill="red", outline="black")
+    # ball = canvas.create_oval(900, 500, 1000, 600, fill="green", width=5)
 
 
-def move():
-    canvas.move(ball, -1, -1)
-    canvas.after(10, move)
+    # def move():
+    #     canvas.move(ball, -1, -1)
+    #     canvas.after(10, move)
+    # move()
 
 
-move()
+    def colors():
+        rand_hexagon = random.randint(0, len(figures) - 1)
+        rand_color = random.randint(0, len(field_colors) - 1)
+        canvas.itemconfigure(figures[rand_hexagon], fill=field_colors[rand_color])
+        canvas.after(10, colors)
+    colors()
 
-
-def colors():
-    rand_hexagon = random.randint(0, len(hexagons) - 1)
-    rand_color = random.randint(0, len(field_colors) - 1)
-    canvas.itemconfigure(hexagons[rand_hexagon], fill=field_colors[rand_color])
-    canvas.after(10, colors)
-
-
-colors()
-
-# canvas.create_polygon(0, 0, 0, 75, 75, 75, 75, 0, fill="red")
-# canvas.create_oval(100, 100, 300, 300, fill="green")
-# canvas.create_polygon(100, 100, 150, 150, 200, 100, fill="blue")
-
-# while True:
-#     for i in range((mx-100)//10):
-#         c.move(ball, 10, 1)
-#         root.update()
-#         time.sleep(0.0005)
-
+hexBtn = ttk.Button(text="Hexagon field", command=print_hex)
+hexBtn.pack()
 
 root.mainloop()
